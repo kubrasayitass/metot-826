1db8df5ee9f8d4d26887d69081663797a9bdbc33
kubrasayitass <145451189+kubrasayitass@users.noreply.github.com>
2024-02-05 12:05:04 +0300

    Add files via upload

---
+++ WindowsFormsApp2/Sekil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    internal class Sekil
+    {
+        public int uzunkenar { get; set; }
+        public int kisakenar { get; set; }
+
+
+
+        public void kareyaz()
+        {
+            int sonuc = this.uzunkenar*this.kisakenar;
+            MessageBox.Show($"Şeklin Karesi:{sonuc}");
+
+        }
+        public void sekilCiz(Button btn,Color  renk)
+        {
+            btn.Width = this.uzunkenar;
+            btn.Height = this.kisakenar;
+            btn.BackColor = renk;
+        }
+    }
+}
